5686c7487e74e81b4ca8ec47433ce25d0d4845aa
Andrey <andrey@dpromo.su>
2018-06-28 14:35:40 +0300

    Change зщке

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ app.get('/', (req,res) => {
     res.send({hi: 'there'}); 
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(5000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", function() {
+    console.log("Listening on Port 3000");
+});
